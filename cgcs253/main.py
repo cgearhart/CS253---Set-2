ca19ddf791feaf8034ae00983b841e4287f8d707
CGearhart <cgearhart@gmail.com>
2012-11-13 01:19:13 -0500

    Working version

--- cgcs253/main.py
+++ cgcs253/main.py
@@ -15,10 +15,50 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+
+form = """
+    <form method="post">
+    Enter some text to ROT13.
+    <br>
+    <textarea name="text" rows="10" cols="80">%(user_text)s</textarea>
+    <br>
+    <br>
+    <input type="submit">
+    </form>
+    """
+
+from_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+to_characters = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+trans_table = dict(zip(from_characters,to_characters))
+
+def rot13(s):
+    s2 = ''
+    
+    for letter in s:
+        if letter in from_characters:
+            s2 += trans_table[letter]
+        else:
+            s2 += letter
+
+    return escape_html(s2)
+
+
+def escape_html(s):
+    return cgi.escape(s,quote=True)
 
 class MainHandler(webapp2.RequestHandler):
+    def write_form(self,text=""):
+        self.response.write(form % {"user_text": text})
+    
     def get(self):
-        self.response.write('Hello world!')
+        self.write_form()
+
+    def post(self):
+        text = self.request.get('text')
+
+        text = rot13(text)
+        self.write_form(text=text)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
